05cdbecb2c5df517a6a5a371bbad54d680230623
nnarziev95 <nnarziev@gmail.com>
2020-03-08 22:42:46 +0900

    Initial commit

---
+++ app/src/main/java/com/nematjon/socialapp/CreateAccount.kt
@@ -0,0 +1,13 @@
+package com.nematjon.socialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccount : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+    }
+}
